2dd67ce10b20aca264efb19753a00cf9dcd56e21
Muhammad-Mehedi-Hasan-Opi-Sarker <opisarker99@gmail.com>
2023-04-14 14:43:38 +0600

    solved Even_and_Odd

---
+++ assingment_2/Even_and_Odd.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, V, i;
+    scanf("%d", &N);
+    int sumEven = 0;
+    int sumOdd= 0;
+    for (i = 1; i <= N; i++)
+    {
+        scanf("%d", &V);
+        if (V % 2 == 0)
+        {
+            sumEven = sumEven + V;
+        }
+        else if (V%2==1)
+        {
+            sumOdd = sumOdd +V;
+        }
+        
+    }
+    printf("%d ", sumEven);
+    printf("%d", sumOdd);
+    return 0;
+}
