0f7d325a80d931d2dc1debcb5bad8a01984f348f
Muhammad-Mehedi-Hasan-Opi-Sarker <opisarker99@gmail.com>
2023-05-10 10:27:14 +0600

    added sum calculation

---
+++ Multidimensional Array/sum.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main()
+{
+    int m, n;
+    scanf("%d %d", &m, &n);
+    int matrix1[m][n], matrix2[m][n], result[m][n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    /* for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d\t", matrix1[i][j]);
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d\t", matrix2[i][j]);
+        }
+        printf("\n");
+
+    } */
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+        printf("\n");
+    }
+
+    printf("sum of the matrix\n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d\t", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
