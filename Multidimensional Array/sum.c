a922b70456bdefa0ebd92cb6826221c3358518a5
Muhammad-Mehedi-Hasan-Opi-Sarker <opisarker99@gmail.com>
2023-05-20 22:12:48 +0600

    check for

--- Multidimensional Array/sum.c
+++ Multidimensional Array/sum.c
@@ -30,7 +30,7 @@ int main()
         printf("\n");
     }
 
-    printf("sum of the matrix\n");
+    // printf("sum of the matrix\n");
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
